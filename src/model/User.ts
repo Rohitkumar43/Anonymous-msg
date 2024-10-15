810eb3d5e1bfafc9c0f8edc6fd1c404cd1dd7c63
rohit kumar <kumarrohit9337@gmail.com>
2024-10-15 13:37:08 +0530

    Done

---
+++ src/model/User.ts
@@ -0,0 +1,72 @@
+import mongoose , {Schema , Document} from "mongoose";
+
+
+export interface Message extends Document  {
+    content: string,
+    createdAt: Date
+}
+
+const messageSchema :  Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+// interface for the user 
+export interface User extends Document  {
+    username: string,
+    email: string,
+    password: string,
+    verifycode: string,
+    verifycodeexpirary: string,
+    isverified: boolean,
+    isacceptingmsg: boolean,
+    messages: [Message]
+}
+// schema for the user
+const UserSchema :  Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true , 'Username is required'],
+        trim: true,
+        unique: true
+
+    },
+    email: {
+        type: String,
+        required: [true , "email is required "],
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/ , "please provide a valid email "]
+    },
+    password: {
+        type: String,
+        required: [true , "password is required"],
+        unique: true
+    },
+    verifycode: {
+        type: String,
+        required: true,
+    },
+    verifycodeexpirary: {
+        type: String,
+        required: [true , "verify code expirary is required "]
+    },
+    isverified: {
+        type: Boolean,
+        required: false
+    },
+    isacceptingmsg: {
+        type: Boolean,
+        required: true
+    },
+    messages: [messageSchema]
+});
+// here we export the model and for the type safety we have to define the schema 
+const Usermodel = (mongoose.models.User as mongoose.Model<User> ) || (mongoose.model<User>("User" , UserSchema))
+
+export default Usermodel;
