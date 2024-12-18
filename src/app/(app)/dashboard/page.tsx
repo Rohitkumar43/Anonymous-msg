5f4cc6cd799f3f9afe68bd2ae2d9e850e8d5bf17
rohit kumar <kumarrohit9337@gmail.com>
2024-12-18 17:33:44 +0530

    Done

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,39 @@
-import React from 'react'
+'use client';
+
+import { MessageCard } from '@/components/MsgCard';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Switch } from '@/components/ui/switch';
+import { useToast } from '@/hooks/use-toast';
+import { Message } from '@/model/User';
+import { apiResponse } from '@/types/apiresponses';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
+import { Loader2, RefreshCcw } from 'lucide-react';
+import { User } from 'next-auth';
+import { useSession } from 'next-auth/react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { AcceptMessageSchema } from '@/schemas/acceptMessageSchema';
 
 const dashboard = () => {
+  // all the states are defined
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false);
+
+  const { toast } = useToast();
+// fxn to delte the msg also this si used for the optimistic UI menas it doesn't del immedatily server fxn after some time 
+  const handleDeleteMessage = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id !== messageId));
+  };
+
+  const { data: session } = useSession();
+
+  const form = useForm({
+    resolver: zodResolver(AcceptMessageSchema),
+  });
+
   return (
     <div>
       dashboard
