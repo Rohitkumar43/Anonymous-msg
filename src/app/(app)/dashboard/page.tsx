ccc1f296267740ddc60ff9d53c590d4ea5a541b2
rohit kumar <kumarrohit9337@gmail.com>
2024-12-31 03:13:58 +0530

    Done

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -14,7 +14,7 @@ import { User } from 'next-auth';
 import { useSession } from 'next-auth/react';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { AcceptMessageSchema } from '@/schemas/acceptMessageSchema';
+import {acceptMessageSchema } from '@/Schemas/acceptMessageSchema';
 
 const dashboard = () => {
   // all the states are defined
@@ -31,8 +31,86 @@ const dashboard = () => {
   const { data: session } = useSession();
 
   const form = useForm({
-    resolver: zodResolver(AcceptMessageSchema),
+    resolver: zodResolver(acceptMessageSchema),
   });
+  // take the value from the form 
+  const {register , watch , setValue} = form
+
+  const acceptMessages = watch('acceptMessages');
+  
+  // calling all the api where we get all the message 
+   const fetchAcceptMessage = useCallback(async() => {
+    // start the loading
+    try {
+      const response = await axios.get('/api/accept-msg')
+      // set the value if ant changes
+      setValue('acceptMessages' , response.data.isacceptingmsg)
+
+    } catch (error) {
+      const AxiosError = error as AxiosError<apiResponse>
+      toast({
+        title: "Error",
+        description: AxiosError.response?.data.message ||
+        "Failed to fetch message setting ",
+        variant: 'destructive'
+      });
+      
+    } finally {
+      setIsSwitchLoading(false);
+    }
+
+   } , [setValue])
+
+
+// for calling the the fetch message of me only 
+const fetchMessage = useCallback(async(refresh : boolean = false ) => {
+  setIsLoading(true);
+  setIsSwitchLoading(false)
+  try {
+    const response = await axios.get<apiResponse>('/api/get-msg');
+    setMessages(response.data.messages || [])
+    if(refresh){
+      toast({
+        title: "Refeshed Message",
+        description: 'Showing latest Message '
+      })
+    }
+
+  } catch (error) {
+    const AxiosError = error as AxiosError<apiResponse>
+      toast({
+        title: "Error",
+        description: AxiosError.response?.data.message ||
+        "Failed to fetch message setting ",
+        variant: 'destructive'
+      });
+  } finally {
+    setIsLoading(false);
+    setIsSwitchLoading(false);
+  }
+} , [setValue , setIsLoading , setMessages])
+
+//
+useEffect(() => {
+  // check the session forst 
+  if (!session || !session.user) {
+    return 
+    fetchMessage()
+    fetchAcceptMessage()
+    
+  }
+
+}, [session , setValue , fetchAcceptMessage , fetchMessage])
+
+
+
+
+
+
+
+
+
+
 
   return (
     <div>
